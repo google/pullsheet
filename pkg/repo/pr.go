a2310a3b791236f5ff101c321753fd9d07c312be
Thomas Stromberg <tstromberg@google.com>
2021-02-24 22:10:57 +0000

    Make PR review comment stats stable

--- pkg/repo/pr.go
+++ pkg/repo/pr.go
@@ -1,16 +1,11 @@
 package repo
 
 import (
-	"bufio"
 	"context"
-	"fmt"
-	"path/filepath"
 	"regexp"
 	"strings"
 	"time"
-	"unicode"
 
-	"github.com/blevesearch/segment"
 	"github.com/google/go-github/v33/github"
 	"github.com/google/pullsheet/pkg/ghcache"
 	"github.com/peterbourgon/diskv"
@@ -23,29 +18,7 @@ var (
 	ignorePathRe = regexp.MustCompile(`go\.mod|go\.sum|vendor/|third_party|ignore|schemas/v\d|schema/v\d|Gopkg.lock|.DS_Store`)
 	truncRe      = regexp.MustCompile(`changelog|CHANGELOG|Gopkg.toml`)
 	commentRe    = regexp.MustCompile(`<!--.*?>`)
-	notSegmentRe = regexp.MustCompile(`[/-_]+`)
-	tagRe        = regexp.MustCompile(`/[\w-]+`)
 )
-
-// FilteredFiles returns a filtered list of files modified by a PR
-func FilteredFiles(ctx context.Context, _ *diskv.Diskv, c *github.Client, org string, project string, num int) ([]*github.CommitFile, error) {
-	klog.Infof("Fetching file list for #%d", num)
-
-	var files []*github.CommitFile
-	changed, _, err := c.PullRequests.ListFiles(ctx, org, project, num, &github.ListOptions{})
-	if err != nil {
-		return files, err
-	}
-
-	for _, cf := range changed {
-		if ignorePathRe.MatchString(cf.GetFilename()) {
-			klog.Infof("ignoring %s", cf.GetFilename())
-			continue
-		}
-		files = append(files, cf)
-	}
-	return files, err
-}
 
 // MergedPulls returns a list of pull requests in a project
 func MergedPulls(ctx context.Context, dv *diskv.Diskv, c *github.Client, org string, project string, since time.Time, until time.Time, users []string) ([]*github.PullRequest, error) {
@@ -103,7 +76,7 @@ func MergedPulls(ctx context.Context, dv *diskv.Diskv, c *github.Client, org str
 				continue
 			}
 
-			klog.Infof("Fetching PR #%d by %s: %q", pr.GetNumber(), pr.GetUser().GetLogin(), pr.GetTitle())
+			klog.Infof("Fetching PR #%d by %s (updated %s): %q", pr.GetNumber(), pr.GetUser().GetLogin(), pr.GetUpdatedAt(), pr.GetTitle())
 
 			fullPR, err := ghcache.PullRequestsGet(ctx, dv, c, pr.GetMergedAt(), org, project, pr.GetNumber())
 			if err != nil {
@@ -127,157 +100,6 @@ func MergedPulls(ctx context.Context, dv *diskv.Diskv, c *github.Client, org str
 	}
 	klog.Infof("Returning %d pull request results", len(result))
 	return result, nil
-}
-
-// ReviewSummary a summary of a users reviews on a PR
-type ReviewSummary struct {
-	URL            string
-	Date           string
-	Reviewer       string
-	PRAuthor       string
-	Project        string
-	Title          string
-	PRComments     int
-	ReviewComments int
-	Words          int
-}
-
-type comment struct {
-	Author    string
-	Body      string
-	Review    bool
-	CreatedAt time.Time
-}
-
-// MergedReviews returns a list of pull requests in a project (merged only)
-func MergedReviews(ctx context.Context, dv *diskv.Diskv, c *github.Client, org string, project string, since time.Time, until time.Time, users []string) ([]*ReviewSummary, error) {
-	prs, err := MergedPulls(ctx, dv, c, org, project, since, until, nil)
-	if err != nil {
-		return nil, fmt.Errorf("pulls: %v", err)
-	}
-
-	popts := &github.PullRequestListCommentsOptions{
-		ListOptions: github.ListOptions{
-			PerPage: 100,
-		},
-	}
-
-	iopts := &github.IssueListCommentsOptions{
-		ListOptions: github.ListOptions{
-			PerPage: 100,
-		},
-	}
-
-	klog.Infof("found %d PR's to check reviews for", len(prs))
-	reviews := []*ReviewSummary{}
-
-	matchUser := map[string]bool{}
-	for _, u := range users {
-		matchUser[strings.ToLower(u)] = true
-	}
-
-	for _, pr := range prs {
-		// username -> summary
-		prMap := map[string]*ReviewSummary{}
-		comments := []comment{}
-
-		// There is wickedness in the GitHub API: PR comments are available via the Issues API, and PR *review* comments are available via the PullRequests API
-		cs, _, err := c.PullRequests.ListComments(ctx, org, project, pr.GetNumber(), popts)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, c := range cs {
-			if isBot(c.GetUser()) {
-				continue
-			}
-			comments = append(comments, comment{Author: c.GetUser().GetLogin(), Body: c.GetBody(), CreatedAt: c.GetCreatedAt(), Review: true})
-		}
-
-		is, _, err := c.Issues.ListComments(ctx, org, project, pr.GetNumber(), iopts)
-		if err != nil {
-			return nil, err
-		}
-		for _, i := range is {
-			if isBot(i.GetUser()) {
-				continue
-			}
-			comments = append(comments, comment{Author: i.GetUser().GetLogin(), Body: i.GetBody(), CreatedAt: i.GetCreatedAt(), Review: false})
-		}
-
-		for _, c := range comments {
-			if len(matchUser) > 0 && !matchUser[strings.ToLower(c.Author)] {
-				continue
-			}
-
-			if c.Author == pr.GetUser().GetLogin() {
-				continue
-			}
-
-			if prMap[c.Author] == nil {
-				prMap[c.Author] = &ReviewSummary{
-					URL:      pr.GetHTMLURL(),
-					PRAuthor: pr.GetUser().GetLogin(),
-					Reviewer: c.Author,
-					Project:  project,
-					Title:    strings.TrimSpace(pr.GetTitle()),
-				}
-			}
-			if c.Review {
-				prMap[c.Author].ReviewComments++
-			} else {
-				prMap[c.Author].PRComments++
-			}
-
-			prMap[c.Author].Date = c.CreatedAt.Format(dateForm)
-			words := wordCount(c.Body)
-			prMap[c.Author].Words += words
-			klog.Infof("%d word comment by %s: %q for %s/%s #%d", words, c.Author, strings.TrimSpace(c.Body), org, project, pr.GetNumber())
-		}
-
-		for _, rs := range prMap {
-			reviews = append(reviews, rs)
-		}
-	}
-
-	return reviews, err
-}
-
-// wordCount counts words in a string, irrespective of language
-func wordCount(s string) int {
-	// Don't count certain items, like / or - as word segments
-	s = notSegmentRe.ReplaceAllString(s, "")
-
-	words := 0
-	scanner := bufio.NewScanner(strings.NewReader(s))
-	scanner.Split(segment.SplitWords)
-	for scanner.Scan() {
-		if !unicode.IsLetter(rune(scanner.Bytes()[0])) {
-			continue
-		}
-		words++
-	}
-	return words
-}
-
-func isBot(u *github.User) bool {
-	if u.GetType() == "bot" {
-		return true
-	}
-
-	if strings.Contains(u.GetBio(), "stale issues") {
-		return true
-	}
-
-	if strings.HasSuffix(u.GetLogin(), "-bot") || strings.HasSuffix(u.GetLogin(), "-robot") || strings.HasSuffix(u.GetLogin(), "_bot") || strings.HasSuffix(u.GetLogin(), "_robot") {
-		return true
-	}
-
-	if strings.HasPrefix(u.GetLogin(), "codecov") || strings.HasPrefix(u.GetLogin(), "Travis") {
-		return true
-	}
-
-	return false
 }
 
 // PRSummary is a summary of a single PR
@@ -368,48 +190,4 @@ func PullSummary(prs map[*github.PullRequest][]*github.CommitFile, since time.Ti
 		})
 	}
 	return sum, nil
-}
-
-// prType returns what kind of PR it thinks this may be
-func prType(files []*github.CommitFile) string {
-	result := ""
-	for _, cf := range files {
-		f := cf.GetFilename()
-		ext := strings.TrimLeft(filepath.Ext(f), ".")
-
-		if strings.Contains(filepath.Dir(f), "docs/") || strings.Contains(filepath.Dir(f), "examples/") || strings.Contains(filepath.Dir(f), "site/") {
-			if result == "" {
-				result = "docs"
-			}
-			klog.Infof("%s: %s", f, result)
-			continue
-		}
-
-		if strings.Contains(f, "test") || strings.Contains(f, "integration") {
-			if result == "" {
-				result = "tests"
-			}
-			klog.Infof("%s: %s", f, result)
-			continue
-		}
-
-		if ext == "md" && result == "" {
-			result = "docs"
-		}
-
-		if ext == "go" || ext == "java" || ext == "cpp" || ext == "py" || ext == "c" || ext == "rs" {
-			result = "backend"
-		}
-
-		if ext == "ts" || ext == "js" || ext == "html" {
-			result = "frontend"
-		}
-
-		klog.Infof("%s (ext=%s): %s", f, ext, result)
-	}
-
-	if result == "" {
-		return "unknown"
-	}
-	return result
 }
